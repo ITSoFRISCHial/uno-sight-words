a82cdc1b5f332218a3a27abb626162169e8aef65
Jeremy Frisch <ITSoFRISCHial@users.noreply.github.com>
2026-01-13 15:36:20 -0600

    feat(frontend): serve build in web service

---
+++ packages/unoenty/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const port = process.env.PORT || 10000
+const buildPath = path.join(__dirname, "build")
+
+app.use(express.static(buildPath))
+
+app.get("*", (_req, res) => {
+	res.sendFile(path.join(buildPath, "index.html"))
+})
+
+app.listen(port, "0.0.0.0", () => {
+	// eslint-disable-next-line no-console
+	console.log(`Listening on ${port}`)
+})
